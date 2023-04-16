ee823385ab2a0881ca40379a0b1563d2dda75469
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-16 10:51:22 -0700

    04

--- if04.py
+++ if04.py
@@ -8,4 +8,20 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    if a>0:
+        x=a//a
+    if a<0:
+        x=0
+    if b>0:
+        x=b//b+x
+    if b<0:
+        x=0
+    if c>0:
+        x=c//c+x
+    if c<0:
+        x=0
+    return x
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+print(main(a,b,c))
